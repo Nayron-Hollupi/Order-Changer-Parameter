c786bbbbf230b4b5cce53207affebe7df2dbf069
Nayron <nhollupi@gmail.com>
2021-03-15 18:33:47 -0300

    Migration

---
+++ Order-Changer-Parameter/database/migrations/2021_03_15_212044_create_maquina_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaquinaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maquina', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Setor');
+            $table->string('Tag');
+            $table->string('Maquina');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maquina');
+    }
+}
---
+++ Order-Changer-Parameter/database/migrations/2021_03_15_212109_create_ordem_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ordem', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Setor');
+            $table->string('Maquina');
+            $table->string('Registro_do_tecnico');
+            $table->string('Causa_do_problema');
+            $table->string('Data_inicio');
+            $table->string('Data_fim');
+            $table->string('Laudo_tecnico');
+            $table->string('Peças_utilizadas');
+            $table->string('Observações');
+            $table->string('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ordem');
+    }
+}
